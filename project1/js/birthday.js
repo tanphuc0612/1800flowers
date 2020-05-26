70f4828e3de40c43a6924f6f8f2e60dd8fa0b569
tanphuc06 <nguyendoantanphuc@gmail.com>
2020-05-26 10:39:38 +0700

    create

---
+++ project1/js/birthday.js
@@ -0,0 +1,242 @@
+﻿// JavaScript source code
+$(document).ready(function () {
+    //xử lí birthday-flowers-caption
+    $('#birthday-flowers-p-hide').hide();
+    $('#close-more').hide();
+    $('#read-more').click(function () {
+        $('#birthday-flowers-p-hide').show();
+        $('#close-more').show();
+        $('#read-more').hide();
+    });
+    $('#close-more').click(function () {
+        $('#birthday-flowers-p-hide').hide();
+        $('#close-more').hide();
+        $('#read-more').show();
+    });
+
+    //xử lý menu sort
+    $('#sort-menu').hide();
+    $('#sort-filter').click(function () {
+        $('#sort-menu').toggle();
+    });
+    $('#button-sort-1').css('background-color', '#5e3987');
+    $('#button-sort-1').css('color', 'white');
+    var button1 = 1;
+    var button2 = 0;
+    var button3 = 0;
+    $('#button-sort-1').click(function () {
+        $('#button-sort-1').css('background-color', '#5e3987');
+        $('#button-sort-1').css('color', 'white');
+        $('#button-sort-2').css('background-color', 'white');
+        $('#button-sort-2').css('color', 'black');
+        $('#button-sort-3').css('background-color', 'white');
+        $('#button-sort-3').css('color', 'black');
+        button1 = 1;
+        button2 = 0;
+        button3 = 0;
+    });
+    $('#button-sort-2').click(function () {
+        $('#button-sort-2').css('background-color', '#5e3987');
+        $('#button-sort-2').css('color', 'white');
+        $('#button-sort-1').css('background-color', 'white');
+        $('#button-sort-1').css('color', 'black');
+        $('#button-sort-3').css('background-color', 'white');
+        $('#button-sort-3').css('color', 'black');
+        button1 = 0;
+        button2 = 1;
+        button3 = 0;
+    });
+    $('#button-sort-3').click(function () {
+        $('#button-sort-3').css('background-color', '#5e3987');
+        $('#button-sort-3').css('color', 'white');
+        $('#button-sort-2').css('background-color', 'white');
+        $('#button-sort-2').css('color', 'black');
+        $('#button-sort-1').css('background-color', 'white');
+        $('#button-sort-1').css('color', 'black');
+        button1 = 0;
+        button2 = 0;
+        button3 = 1;
+    });
+    $('#button-sort-1').mouseenter(function () {
+        if (button1 == 0) {
+            $('#button-sort-1').css('background-color', '#c1afd1');
+            $('#button-sort-1').css('color', 'white');
+        }
+    });
+    $('#button-sort-1').mouseleave(function () {
+        if (button1 == 0) {
+            $('#button-sort-1').css('background-color', 'white');
+            $('#button-sort-1').css('color', 'black');
+        }
+    });
+    $('#button-sort-2').mouseenter(function () {
+        if (button2 == 0) {
+            $('#button-sort-2').css('background-color', '#c1afd1');
+            $('#button-sort-2').css('color', 'white');
+        }
+    });
+    $('#button-sort-2').mouseleave(function () {
+        if (button2 == 0) {
+            $('#button-sort-2').css('background-color', 'white');
+            $('#button-sort-2').css('color', 'black');
+        }
+    });
+    $('#button-sort-3').mouseenter(function () {
+        if (button3 == 0) {
+            $('#button-sort-3').css('background-color', '#c1afd1');
+            $('#button-sort-3').css('color', 'white');
+        }
+    });
+    $('#button-sort-3').mouseleave(function () {
+        if (button3 == 0) {
+            $('#button-sort-3').css('background-color', 'white');
+            $('#button-sort-3').css('color', 'black');
+        }
+    });
+    var checkbox = 0;
+    $('input[type="checkbox"]').click(function () {
+        if ($(this).prop("checked") == true) {
+            $('#submit-sort').css('color', '#5e3987');
+            $('#submit-sort').css('border', 'solid 2px #5e3987');
+            checkbox++;
+        }
+        else if ($(this).prop("checked") == false) {
+            checkbox--;
+            if (checkbox == 0) {
+                $('#submit-sort').css('color', 'lightgray');
+                $('#submit-sort').css('border', 'solid 1px lightgray');
+            }
+        }
+    });
+    //xử lý minus plus
+    $('#content-1').hide();
+    $('#minus1').hide();
+    $('#plus1').click(function () {
+        $('#content-1').show();
+        $('#plus1').hide();
+        $('#minus1').show();
+    });
+    $('#minus1').click(function () {
+        $('#content-1').hide();
+        $('#plus1').show();
+        $('#minus1').hide();
+    });
+
+    $('#content-2').hide();
+    $('#minus2').hide();
+    $('#plus2').click(function () {
+        $('#content-2').show();
+        $('#plus2').hide();
+        $('#minus2').show();
+    });
+    $('#minus2').click(function () {
+        $('#content-2').hide();
+        $('#plus2').show();
+        $('#minus2').hide();
+    });
+
+
+    $('#content-3').hide();
+    $('#minus3').hide();
+    $('#plus3').click(function () {
+        $('#content-3').show();
+        $('#plus3').hide();
+        $('#minus3').show();
+    });
+    $('#minus3').click(function () {
+        $('#content-3').hide();
+        $('#plus3').show();
+        $('#minus3').hide();
+    });
+
+
+    $('#more-about-birthday-flowers-content-1').hide();
+    $('#minus4').hide();
+    $('#plus4').click(function () {
+        $('#more-about-birthday-flowers-content-1').show();
+        $('#plus4').hide();
+        $('#minus4').show();
+    });
+    $('#minus4').click(function () {
+        $('#more-about-birthday-flowers-content-1').hide();
+        $('#plus4').show();
+        $('#minus4').hide();
+    });
+
+    $('#more-about-birthday-flowers-content-2').hide();
+    $('#minus').hide();
+    $('#plus').click(function () {
+        $('#more-about-birthday-flowers-content-2').show();
+        $('#plus').hide();
+        $('#minus').show();
+    });
+    $('#minus').click(function () {
+        $('#more-about-birthday-flowers-content-2').hide();
+        $('#plus').show();
+        $('#minus').hide();
+    });
+
+    //sort mobile
+    $('#sort-menu-mobile-container').hide();
+    $('#sort-mobile').click(function () {
+        $('#sort-menu-mobile-container').show();
+    });
+    $('#background-sort-menu-mobile').click(function () {
+        $('#sort-menu-mobile').hide();
+        $('#background-sort-menu-mobile').hide();
+    });
+
+    $('#button-sort-1-mobile').css('background-color', '#5e3987');
+    $('#button-sort-1-mobile').css('color', 'white');
+    var button1_mobile = 1;
+    var button2_mobile  = 0;
+    var button3_mobile  = 0;
+    $('#button-sort-1-mobile').click(function () {
+        $('#button-sort-1-mobile').css('background-color', '#5e3987');
+        $('#button-sort-1-mobile').css('color', 'white');
+        $('#button-sort-2-mobile').css('background-color', 'white');
+        $('#button-sort-2-mobile').css('color', 'black');
+        $('#button-sort-3-mobile').css('background-color', 'white');
+        $('#button-sort-3-mobile').css('color', 'black');
+        button1_mobile = 1;
+        button2_mobile = 0;
+        button3_mobile = 0;
+    });
+    $('#button-sort-2-mobile').click(function () {
+        $('#button-sort-2-mobile').css('background-color', '#5e3987');
+        $('#button-sort-2-mobile').css('color', 'white');
+        $('#button-sort-1-mobile').css('background-color', 'white');
+        $('#button-sort-1-mobile').css('color', 'black');
+        $('#button-sort-3-mobile').css('background-color', 'white');
+        $('#button-sort-3-mobile').css('color', 'black');
+        button1_mobile = 0;
+        button2_mobile = 1;
+        button3_mobile = 0;
+    });
+    $('#button-sort-3-mobile').click(function () {
+        $('#button-sort-3-mobile').css('background-color', '#5e3987');
+        $('#button-sort-3-mobile').css('color', 'white');
+        $('#button-sort-2-mobile').css('background-color', 'white');
+        $('#button-sort-2-mobile').css('color', 'black');
+        $('#button-sort-1-mobile').css('background-color', 'white');
+        $('#button-sort-1-mobile').css('color', 'black');
+        button1_mobile = 0;
+        button2_mobile = 0;
+        button3_mobile = 1;
+    });
+    var checkbox_mobile = 0;
+    $('input[type="checkbox"]').click(function () {
+        if ($(this).prop("checked") == true) {
+            $('#submit-sort-mobile').css('color', '#5e3987');
+            $('#submit-sort-mobile').css('border', 'solid 2px #5e3987');
+            checkbox_mobile++;
+        }
+        else if ($(this).prop("checked") == false) {
+            checkbox_mobile--;
+            if (checkbox_mobile == 0) {
+                $('#submit-sort-mobile').css('color', 'lightgray');
+                $('#submit-sort-mobile').css('border', 'solid 1px lightgray');
+            }
+        }
+    });
+});
---
+++ project1/js/home.js
@@ -0,0 +1,312 @@
+﻿// JavaScript source code
+$(document).ready(function () {
+    //xử lí detail ẩn hiện
+    $('.list-trending').mouseenter(function () {
+        $(this).find('.view-detail').css('background-color','#5e3987');
+    });
+    $('.list-trending').mouseleave(function () {
+        $(this).find('.view-detail').css('background-color', 'white');
+    });
+    //xử lí submit email
+    $('#email-submit').find('img').click(function () {
+        var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+        if (!filter.test($('#email-submit').find('input').val())) {
+            alert('Hay nhap dia chi email hop le.\nExample@gmail.com');
+            return false;
+        }
+        else {
+            alert('OK roi day, Email nay hop le.');
+        }
+    });
+    //xử lí get date tự động
+    var date = new Date;
+    var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var maxDate = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    var day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saruday"]
+    var dayName = "";
+    var stt = date.getDate();
+    var j = 0;
+    for (var i = 0; i < 7; i++) {
+        dayName = "<option>" + day[(date.getDay() + i) % 7] + " " + month[date.getMonth() + j] + " " + stt++ + "th</option>";
+        $('#date').append(dayName);
+        $('#date-2').append(dayName);
+        if (maxDate[date.getMonth()] < stt) { j++; stt = 1;}
+    }
+    //xử lí feedback
+    $('#feedback').slideToggle(0);
+    $('#feedback-button').click(function () {
+        $('#feedback').slideToggle(500);
+    });
+    //xử lí dropdown-first
+    $('#flag-menu').hide();
+    $('#header-first-menu').find('.dropdown-toggle').click(function () {
+        if (screen.availWidth > 1000) { $('#flag-menu').toggle(); } 
+    });
+    $('#header-first-menu').find('.dropdown-toggle').blur(function () {
+        $('#flag-menu').hide();
+    });
+    //xử lí dropdown mega
+    $('#spring').hide();
+    $('#spring-menu').mouseenter(function () {
+        $('#spring').show();
+    });
+    $('#spring-menu').mouseleave(function () {
+        $('#spring').hide(50);
+        $('#spring').mouseenter(function () {
+            $('#spring').stop();
+            $('#spring').show();
+        });
+        $('#spring').mouseleave(function () {
+            $('#spring').hide();
+        });
+    });
+
+    $('#easter').hide();
+    $('#easter-menu').mouseenter(function () {
+        $('#easter').show();
+    });
+    $('#easter-menu').mouseleave(function () {
+        $('#easter').hide(50);
+        $('#easter').mouseenter(function () {
+            $('#easter').stop();
+            $('#easter').show();
+        });
+        $('#easter').mouseleave(function () {
+            $('#easter').hide();
+        });
+    });
+
+
+    $('#birthday').hide();
+    $('#birthday-menu').mouseenter(function () {
+        $('#birthday').show();
+    });
+    $('#birthday-menu').mouseleave(function () {
+        $('#birthday').hide(50);
+        $('#birthday').mouseenter(function () {
+            $('#birthday').stop();
+            $('#birthday').show();
+        });
+        $('#birthday').mouseleave(function () {
+            $('#birthday').hide();
+        });
+    });
+
+    $('#sympathy').hide();
+    $('#sympathy-menu').mouseenter(function () {
+        $('#sympathy').show();
+    });
+    $('#sympathy-menu').mouseleave(function () {
+        $('#sympathy').hide(50);
+        $('#sympathy').mouseenter(function () {
+            $('#sympathy').stop();
+            $('#sympathy').show();
+        });
+        $('#sympathy').mouseleave(function () {
+            $('#sympathy').hide();
+        });
+    });
+
+    $('#occasions').hide();
+    $('#occasions-menu').mouseenter(function () {
+        $('#occasions').show();
+    });
+    $('#occasions-menu').mouseleave(function () {
+        $('#occasions').hide(50);
+        $('#occasions').mouseenter(function () {
+            $('#occasions').stop();
+            $('#occasions').show();
+        });
+        $('#occasions').mouseleave(function () {
+            $('#occasions').hide();
+        });
+    });
+
+    $('#flowers').hide();
+    $('#flowers-menu').mouseenter(function () {
+        $('#flowers').show();
+    });
+    $('#flowers-menu').mouseleave(function () {
+        $('#flowers').hide(50);
+        $('#flowers').mouseenter(function () {
+            $('#flowers').stop();
+            $('#flowers').show();
+        });
+        $('#flowers').mouseleave(function () {
+            $('#flowers').hide();
+        });
+    });
+
+    $('#plants').hide();
+    $('#plants-menu').mouseenter(function () {
+        $('#plants').show();
+    });
+    $('#plants-menu').mouseleave(function () {
+        $('#plants').hide(50);
+        $('#plants').mouseenter(function () {
+            $('#plants').stop();
+            $('#plants').show();
+        });
+        $('#plants').mouseleave(function () {
+            $('#plants').hide();
+        });
+    });
+
+    $('#gift').hide();
+    $('#gift-menu').mouseenter(function () {
+        $('#gift').show();
+    });
+    $('#gift-menu').mouseleave(function () {
+        $('#gift').hide(50);
+        $('#gift').mouseenter(function () {
+            $('#gift').stop();
+            $('#gift').show();
+        });
+        $('#gift').mouseleave(function () {
+            $('#gift').hide();
+        });
+    });
+
+    $('#same-day').hide();
+    $('#same-day-menu').mouseenter(function () {
+        $('#same-day').show();
+    });
+    $('#same-day-menu').mouseleave(function () {
+        $('#same-day').hide(50);
+        $('#same-day').mouseenter(function () {
+            $('#same-day').stop();
+            $('#same-day').show();
+        });
+        $('#same-day').mouseleave(function () {
+            $('#same-day').hide();
+        });
+    });
+
+    $('#sale').hide();
+    $('#sale-menu').mouseenter(function () {
+        $('#sale').show();
+    });
+    $('#sale-menu').mouseleave(function () {
+        $('#sale').hide(50);
+        $('#sale').mouseenter(function () {
+            $('#sale').stop();
+            $('#sale').show();
+        });
+        $('#sale').mouseleave(function () {
+            $('#sale').hide();
+        });
+    });
+    //xử lý reponsive menu
+    $('#reponsive-menu-container').hide();
+    $('#reponsive-header-bars').click(function () {
+        $('#reponsive-menu-container').show();
+    });
+    $('#reponsive-menu-1-close').click(function () {
+        $('#reponsive-menu-container').hide();
+    });
+    //back-next
+    $('#reponsive-menu-main-birthday').hide();
+    $('#next-birthday').click(function () {
+        $('#reponsive-menu-main-home').hide();
+        $('#reponsive-menu-main-birthday').show();
+    });
+    $('.back-home').click(function () {
+        $('#reponsive-menu-main-birthday').hide();
+        $('#reponsive-menu-main-home').show();
+    });
+
+    $('#reponsive-menu-main-sympathy').hide();
+    $('#next-sympathy').click(function () {
+        $('#reponsive-menu-main-home').hide();
+        $('#reponsive-menu-main-sympathy').show();
+    });
+    $('.back-home').click(function () {
+        $('#reponsive-menu-main-sympathy').hide();
+        $('#reponsive-menu-main-home').show();
+    });
+
+    $('#reponsive-menu-main-occasions').hide();
+    $('#next-occasions').click(function () {
+        $('#reponsive-menu-main-home').hide();
+        $('#reponsive-menu-main-occasions').show();
+    });
+    $('.back-home').click(function () {
+        $('#reponsive-menu-main-occasions').hide();
+        $('#reponsive-menu-main-home').show();
+    });
+
+    $('#reponsive-menu-main-flowers').hide();
+    $('#next-flowers').click(function () {
+        $('#reponsive-menu-main-home').hide();
+        $('#reponsive-menu-main-flowers').show();
+    });
+    $('.back-home').click(function () {
+        $('#reponsive-menu-main-flowers').hide();
+        $('#reponsive-menu-main-home').show();
+    });
+
+    $('#reponsive-menu-main-plants').hide();
+    $('#next-plants').click(function () {
+        $('#reponsive-menu-main-home').hide();
+        $('#reponsive-menu-main-plants').show();
+    });
+    $('.back-home').click(function () {
+        $('#reponsive-menu-main-plants').hide();
+        $('#reponsive-menu-main-home').show();
+    });
+
+    $('#reponsive-menu-main-gift').hide();
+    $('#next-gift').click(function () {
+        $('#reponsive-menu-main-home').hide();
+        $('#reponsive-menu-main-gift').show();
+    });
+    $('.back-home').click(function () {
+        $('#reponsive-menu-main-gift').hide();
+        $('#reponsive-menu-main-home').show();
+    });
+
+    $('#reponsive-menu-main-same-day').hide();
+    $('#next-same-day').click(function () {
+        $('#reponsive-menu-main-home').hide();
+        $('#reponsive-menu-main-same-day').show();
+    });
+    $('.back-home').click(function () {
+        $('#reponsive-menu-main-same-day').hide();
+        $('#reponsive-menu-main-home').show();
+    });
+
+    $('#reponsive-menu-main-sale').hide();
+    $('#next-sale').click(function () {
+        $('#reponsive-menu-main-home').hide();
+        $('#reponsive-menu-main-sale').show();
+    });
+    $('.back-home').click(function () {
+        $('#reponsive-menu-main-sale').hide();
+        $('#reponsive-menu-main-home').show();
+    });
+    //feedback  
+    var check = 0;
+    $('#feedback-button').click(function () {
+        if (check == 0) {
+            $('#feedback-button').animate({ bottom:'326px'},500);
+            check = 1;
+        }
+        else {
+            $('#feedback-button').animate({ bottom: '30px' }, 500);
+            check = 0;
+        }
+    });
+    //xử lý exclusive list
+    $('.exclusive-image').mouseenter(function () {
+        $(this).animate({
+            width: '190px',
+            height:'220px'
+        },300);
+    });
+    $('.exclusive-image').mouseleave(function () {
+        $(this).animate({
+            width: '180px',
+            height: '200px'
+        }, 300);
+    });
+});
